b208258f6eb6376d4b7428a912a7f3f1257c3947
Drew <drewgamesaw@gmail.com>
2016-12-23 22:26:50 -0500

    Chat logging in console

--- toontown/toon/DistributedToon.py
+++ toontown/toon/DistributedToon.py
@@ -493,6 +493,14 @@ class DistributedToon(DistributedPlayer.DistributedPlayer, Toon.Toon, Distribute
         if config.GetBool('want-sleep-reply-on-regular-chat', 0):
             if base.localAvatar.sleepFlag == 1:
                 self.sendUpdate('setSleepAutoReply', [base.localAvatar.doId], fromAV)
+                
+        if fromAV == 0:
+            print("%s: %r" % (self.name, chat))
+            base.chatLog.append("%s: %r" % (self.name, chat))
+        else:
+            print("%s: %r" % (fromAV.name, chat))
+            base.chatLog.append("%s: %r" % (fromAV.name, chat))
+        
         newText, scrubbed = self.scrubTalk(chat, mods)
         self.displayTalk(newText)
         base.talkAssistant.receiveOpenTalk(fromAV, avatarName, fromAC, None, newText)
